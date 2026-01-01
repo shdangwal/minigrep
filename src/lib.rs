61a34df16376b5a5ba618ee785008633714509d8
Shubham Dangwal <shdangwal@gmail.com>
2026-01-01 21:38:28 +0530

    use closures

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,14 @@
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> impl Iterator<Item = &'a str> {
+    contents.lines().filter(move |line| line.contains(&query))
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> impl Iterator<Item = &'a str> {
+    contents
+        .lines()
+        .filter(move |line| line.to_lowercase().contains(&query.to_lowercase()))
 }
 
 #[cfg(test)]
@@ -34,7 +22,11 @@ mod tests {
 Rust:
 safe, fast, productive.
 Pick three.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        let mut v = vec![];
+        for val in search(query, contents) {
+            v.push(val);
+        }
+        assert_eq!(vec!["safe, fast, productive."], v);
     }
 
     #[test]
@@ -46,7 +38,11 @@ safe, fast, productive.
 Pick three.
 Duct tape.";
 
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        let mut v = vec![];
+        for val in search(query, contents) {
+            v.push(val);
+        }
+        assert_eq!(vec!["safe, fast, productive."], v);
     }
 
     #[test]
@@ -58,9 +54,10 @@ safe, fast, productive.
 Pick three.
 Trust me.";
 
-        assert_eq!(
-            vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents)
-        );
+        let mut v = vec![];
+        for val in search_case_insensitive(query, contents) {
+            v.push(val);
+        }
+        assert_eq!(vec!["Rust:", "Trust me."], v);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ use std::process;
 
 fn main() {
     //TODO: look at usage of args_os() instead [https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html#the-args-function-and-invalid-unicode]
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -26,10 +24,10 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    let results = if config.ignore_case {
-        search_case_insensitive(&config.query, &contents)
+    let results: Box<dyn Iterator<Item = &str>> = if config.ignore_case {
+        Box::new(search_case_insensitive(&config.query, &contents))
     } else {
-        search(&config.query, &contents)
+        Box::new(search(&config.query, &contents))
     };
 
     for line in results {
@@ -46,12 +44,18 @@ pub struct Config {
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("MINIGREP_IGNORE_CASE").is_ok();
 
