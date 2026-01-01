e9a6d22db0ec42a79b0a617842c7f32c20fb2766
Shubham Dangwal <shdangwal@gmail.com>
2026-01-01 12:27:14 +0530

    accepting command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    //TODO: look at usage of args_os() instead [https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html#the-args-function-and-invalid-unicode]
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {query}");
+    println!("In file {file_path}");
 }
