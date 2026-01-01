f9cab7b91780a23bf3794af6bfe002f7185cfec1
Shubham Dangwal <shdangwal@gmail.com>
2026-01-01 15:13:29 +0530

    add case_insensitive option

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use minigrep::search;
+use minigrep::search_case_insensitive;
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -25,16 +26,23 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
     Ok(())
 }
 
-struct Config {
-    query: String,
-    file_path: String,
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -45,6 +53,12 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("MINIGREP_IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
