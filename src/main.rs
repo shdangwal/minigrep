7f533583cfdda3439ff6b0092fd6889a14d9c733
Shubham Dangwal <shdangwal@gmail.com>
2026-01-01 12:30:50 +0530

    reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     //TODO: look at usage of args_os() instead [https://doc.rust-lang.org/book/ch12-01-accepting-command-line-arguments.html#the-args-function-and-invalid-unicode]
@@ -9,4 +10,8 @@ fn main() {
 
     println!("Searching for {query}");
     println!("In file {file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Shoud have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
